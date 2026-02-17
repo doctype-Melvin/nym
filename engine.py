8531b6168a80d1e036cf58f71a9bf6d65209a717
Melvin Speckamp <doctype.melvin@gmail.com>
2026-02-17 16:26:18 +0100

    add spacing check to titlecase utility

--- engine.py
+++ engine.py
@@ -26,6 +26,7 @@ def to_titlecase(text):
     def replace_match(match):
         word = match.group(0)
         return word.title() if len(word) >= 4 else word
+    text = re.sub(r'(?<=[A-Z])\s(?=[A-Z])', '', text) 
     return re.sub(r'\b[A-ZÜÖÄß]{3,}\b', replace_match, text)
 
 # Custom Regex
