2347bdae84ab16681d37ccc17d677c5ca8b63ffa
Melvin Speckamp <doctype.melvin@gmail.com>
2026-02-20 12:14:13 +0100

    add current engine implementation

--- post_audit_trail.py
+++ post_audit_trail.py
@@ -18,27 +18,27 @@ def archive_audit_trail(df):
 
         # Integrity has based on SHA-256 fingerprint of event
         # Proof that record wasn't modified
-        hash_string = f'{row["Timestamp"]}{row["Filepath"]}{row.get("PII_Hash", "")}{row["Description"]}'
+        hash_string = f'{row["timestamp"]}{row["filepath"]}{row.get("pii_hash", "")}'
         integrity_hash = hashlib.sha256(hash_string.encode()).hexdigest()
 
         cursor.execute('''
             INSERT INTO audit_trail (
                        record_uuid,
                        filepath,
-                       event_type,
-                       description,
+                       timestamp,
+                       event_code,
                        pii_hash,
                        confidence_score,
                        integrity_hash
                        )
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                       VALUES (?, ?, ?, ?, ?, ?, ?)
                        ''', (
                            str(rec_uuid),
-                           str(row['Filepath']),
-                           str(row['Event_type']),
-                           str(row['Description']),
-                           str(row.get('PII_Hash', '')),
-                           float(row.get('Confidence_Score', 0.0)),
+                           str(row['filepath']),
+                           str(row['timestamp']),
+                           str(row['event_code']),
+                           str(row.get('pii_hash', '')),
+                           float(row.get('confidence_score', 0.0)),
                            integrity_hash
                        ))
         records_added += 1
@@ -49,4 +49,4 @@ def archive_audit_trail(df):
 
 total_saved = archive_audit_trail(log_df)
 
-knio.output_tables[0] = knio.Table.from_pandas(pd.DataFrame([{"Records_Archived": total_saved}]))
+knio.output_tables[0] = knio.Table.from_pandas(pd.DataFrame([{"Records_Archived": total_saved}])) 
